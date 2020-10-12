78e6a15698f0076145b0b4a52f9c80be8614d550
VarunSai23 <imvarun23@gmail.com>
2020-10-13 00:56:29 +0530

    Updates

---
+++ precache-manifest.612c33e0450fade1448a1d8f30cc1ec7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57201eb157abaafbbe732156579e9f05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4397d38f6b25d2b31119",
+    "url": "/static/css/2.fb230578.chunk.css"
+  },
+  {
+    "revision": "7db1c001b87ae457d6e8",
+    "url": "/static/css/main.0073d728.chunk.css"
+  },
+  {
+    "revision": "4397d38f6b25d2b31119",
+    "url": "/static/js/2.3a0ce8bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3a0ce8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db1c001b87ae457d6e8",
+    "url": "/static/js/main.dc06ac4b.chunk.js"
+  },
+  {
+    "revision": "26590c0f50dd680d7c8e",
+    "url": "/static/js/runtime-main.80a3bced.js"
+  },
+  {
+    "revision": "4b68bf6ca547f6540f1d22d76cc5749c",
+    "url": "/static/media/github.4b68bf6c.svg"
+  },
+  {
+    "revision": "00d7cdd34f1b8658a448759901a55748",
+    "url": "/static/media/java.00d7cdd3.svg"
+  },
+  {
+    "revision": "83062fffb1fbc1bb8fa3d86b675d45f4",
+    "url": "/static/media/link.83062fff.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ce6e693d482c54b75de432f170d923cd",
+    "url": "/static/media/me.ce6e693d.jpg"
+  },
+  {
+    "revision": "f3f6059bb3b0fe52da517d8ed5a5bae4",
+    "url": "/static/media/mediasec1.f3f6059b.png"
+  },
+  {
+    "revision": "9017091f18c48f3fb1f671ebcb7b1d69",
+    "url": "/static/media/onealarm1.9017091f.jpg"
+  },
+  {
+    "revision": "cf77ade7f2839d5a19a2864fe6b107ac",
+    "url": "/static/media/onealarm2.cf77ade7.jpg"
+  },
+  {
+    "revision": "bea2ce050a43c8f3a773e57ef1211119",
+    "url": "/static/media/python.bea2ce05.svg"
+  },
+  {
+    "revision": "3eedf37a0f90d3d80a595e380355b167",
+    "url": "/static/media/varsmovies1.3eedf37a.jpg"
+  }
+]);
